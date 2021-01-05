c5a376fa19c84763846f94ef259fb23b6f196778
aleosina12 <68166171+aleosina12@users.noreply.github.com>
2021-01-06 01:44:56 +0300

    Add files via upload

---
+++ Module25.php
@@ -0,0 +1,91 @@
+<?php
+
+abstract class Person
+{
+    public $age;
+    public $name;
+    public $lastName;
+
+    private static $PeopleCount = 0;
+    public static function GetAllPeopleCount()
+    {
+        return static::$PeopleCount;
+    }
+
+    public function __construct(int $Age, string $Name, string $lastName)
+    {
+        self::$PeopleCount++;
+        $this->age = $Age;
+        $this->name = $Name;
+        $this->lastName = $lastName;
+    }
+}
+
+
+class Family
+{
+    private $members = [];
+    public $familyLastname;
+
+    public function getAllMembers()
+    {
+        return $this->members;
+    }
+
+    public function __construct(MyParent $wife, MyParent $husband)
+    {
+        $wife->setSpouse($husband);
+        $husband->setSpouse($wife);
+        $this->members[] = $husband;
+        $this->members[] = $wife;
+        $this->familyLastName = $husband->lastName;
+    }
+
+    public function BearChild() :Person
+    {
+        $child = new Child();
+        $child->father = $this->members[0];
+        $child->mother = $this->members[1];
+        $this->members[] = $child;
+        return $child;
+    }
+
+    public function getMembersCount() :int
+    {
+        return count($this->members);
+    }
+}
+
+class MyParent extends Person
+{
+    public $spouse;
+
+    public function setSpouse(MyParent $newPartner)
+    {
+        $this->spouse = $newPartner;
+    }
+}
+
+class Child extends Person
+{
+    public $sex;
+    public $mother;
+    public $father;
+
+    public function __construct()
+    {
+        $this->age = 0;
+        $this->sex = 'boy';
+    }
+}
+
+
+
+
+$wife1 = new MyParent(42, "Irina", "Potapova");
+$husband1 = new MyParent(42, "Alex", "Osadchuk");
+$family1 = new Family($wife1, $husband1);
+
+
+$child1 = $family1->BearChild();
+echo "Family1 count: " . $family1->getMembersCount();
